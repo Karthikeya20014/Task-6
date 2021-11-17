f55c5f29c8129d76d29dae745879dbe5ed71e8e3
Karthikeya20014 <92802349+Karthikeya20014@users.noreply.github.com>
2021-11-17 11:03:39 +0530

    Create Q2.java

---
+++ Q2.java
@@ -0,0 +1,9 @@
+/*Advertisement Revenue*?
+n = int(input())
+a = [int(i) for i in input().split()]
+b = [int(i) for i in input().split()]
+a.sort()
+b.sort() 
+ans = sum([a[i]*b[i] for i in range(n)])
+print(ans)
+
